49fe6dfc12dca98ad1ce4759d0dbea27840c9ebb
Kazuya-Sakashita <kz0508@gmail.com>
2024-05-29 20:18:47 +0900

    アカウント認証メール機能実装

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -26,7 +26,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_28_095911) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
