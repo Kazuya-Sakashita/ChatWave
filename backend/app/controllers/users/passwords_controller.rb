4f893b1148d0be0a14e8846c9940889f3966904d
Kazuya-Sakashita <kz0508@gmail.com>
2024-06-18 07:28:44 +0900

    user.confirmed_at確認追加

--- backend/app/controllers/users/passwords_controller.rb
+++ backend/app/controllers/users/passwords_controller.rb
@@ -2,29 +2,36 @@ class Users::PasswordsController < Devise::PasswordsController
   respond_to :json
 
   def create
+    user = User.find_by(email: resource_params[:email])
+    if user && user.confirmed_at.nil?
+      log_error("Failed to send reset password instructions to #{user.email}: Email not confirmed")
+      unconfirmed_email
+      return
+    end
+
     self.resource = resource_class.send_reset_password_instructions(resource_params)
     yield resource if block_given?
 
     if successfully_sent?(resource)
-      Rails.logger.info "Reset password instructions sent to #{resource.email}"
+      log_info("Reset password instructions sent to #{resource.email}")
       send_reset_mail_success
     else
-      Rails.logger.error "Failed to send reset password instructions to #{resource.email}: #{resource.errors.full_messages.join(", ")}"
+      log_error("Failed to send reset password instructions to #{resource.email}: #{resource.errors.full_messages.join(", ")}")
       send_reset_mail_failed(resource)
     end
   end
 
   def update
-    logger.info "Received parameters: #{params.inspect}"
+    log_info("Received parameters: #{params.inspect}")
     user_params = params.require(:user).permit(:reset_password_token, :password, :password_confirmation)
-    logger.info "Sanitized parameters: #{user_params.inspect}"
+    log_info("Sanitized parameters: #{user_params.inspect}")
 
     user = User.find_by(reset_password_token: Devise.token_generator.digest(User, :reset_password_token, user_params[:reset_password_token]))
-    logger.info "User found: #{user.inspect}"
+    log_info("User found: #{user.inspect}")
 
     if user.nil?
-      logger.error "Failed to reset password: Reset password token is invalid"
-      render json: { message: 'パスワードのリセットに失敗しました。', errors: ['Reset password token is invalid'] }, status: :unprocessable_entity
+      log_error("Failed to reset password: Reset password token is invalid")
+      reset_password_token_invalid
       return
     end
 
@@ -33,7 +40,7 @@ class Users::PasswordsController < Devise::PasswordsController
       resource.unlock_access! if unlockable?(resource)
       reset_password_success
     else
-      logger.error "Failed to reset password: #{resource.errors.full_messages.join(", ")}"
+      log_error("Failed to reset password: #{resource.errors.full_messages.join(", ")}")
       reset_password_failed(resource)
     end
   end
@@ -64,7 +71,23 @@ class Users::PasswordsController < Devise::PasswordsController
     render json: { message: 'パスワードのリセットに失敗しました。', errors: resource.errors.full_messages }
   end
 
+  def reset_password_token_invalid
+    render json: { message: 'パスワードのリセットに失敗しました。', errors: ['Reset password token is invalid'] }, status: :unprocessable_entity
+  end
+
+  def unconfirmed_email
+    render json: { message: 'メールアドレスが確認されていません。' }, status: :unprocessable_entity
+  end
+
   def resource_params
     params.require(:user).permit(:email, :reset_password_token, :password, :password_confirmation)
+  end
+
+  def log_info(message)
+    Rails.logger.info(message)
+  end
+
+  def log_error(message)
+    Rails.logger.error(message)
   end
 end
