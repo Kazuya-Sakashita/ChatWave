372d78b2fee699bed54f549e842163817cd6283d
Kazuya-Sakashita <kz0508@gmail.com>
2024-08-14 13:36:56 +0900

    性別バリデーション追加

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -8,7 +8,7 @@ class Profile < ApplicationRecord
   # バリデーションの設定
   validates :full_name, presence: true
   validates :birth_date, presence: true
-  validates :gender, presence: true
+  validates :gender, inclusion: { in: %w(male female other), message: "%{value} is not a valid gender" }
   validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
   validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
   validates :address, presence: true
